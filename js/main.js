26320ea7c2203c59a32b21440d5f13d601cba744
Alebex <alex.belost@gmail.com>
2016-03-07 00:46:22 +0200

    start project

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		singleItem : true,
+		itemsScaleUp : false,
+		rewindNav: false
+	});
+	$(".slider__button_next").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".slider__button_prev").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	$('.header__btn').click(function() {
+		$('.nav').slideToggle();
+	});
+
+});
